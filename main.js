4de03908dbb8ea1298e38b0755344c5734512b76
Maja Pokos <maja.pokos@gmail.com>
2022-04-23 12:08:38 +0200

    added functions and keyCodes

--- main.js
+++ main.js
@@ -3,21 +3,43 @@ $(document).ready(function() {
     const $prevButton = $(".slider__button--prev");
     const $nextButton = $(".slider__button--next");
 
+    const keyCodeRight = 39;
+    const keyCodeLeft = 37;
+
     let i = 0;
 
-    $prevButton.click(() => {
+    function clickPrev() {
         if (i > 0) {
             $($slides[i]).fadeOut();
             $($slides[i-1]).fadeIn();
             i--
         }
-    });
+    };
 
-    $nextButton.click(() => {
+    function clickNext() {
         if (i < $slides.length - 1) {
             $($slides[i]).fadeOut();
             $($slides[i+1]).fadeIn();
             i++
         }
+    };
+
+    $prevButton.click(() => {
+        clickPrev();
+    });
+
+    $nextButton.click(() => {
+        clickNext();
+    });
+    
+    $("body").keydown((e) => {
+        if (e.keyCode == keyCodeLeft) {
+        clickPrev();
+        } else if (e.keyCode == keyCodeRight) {
+        clickNext();
+        };
     });
 });
+
+
+
