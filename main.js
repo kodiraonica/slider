f12d08d91e5cc64666f772094351b815f60bbb45
zopaj63 <zoran.pajnic@gmail.com>
2022-04-23 12:09:39 +0200

    add functions Prev and Next

--- main.js
+++ main.js
@@ -2,23 +2,41 @@ $(document).ready(function () {
     const $slides = $(".slide");
     const $prevButton = $(".slider__button--prev");
     const $nextButton = $(".slider__button--next");
-
+    const keyCodeRight = 39;
+    const keyCodeLeft = 37;
     let i = 0;
 
-    $prevButton.click(() => {
-        if (i > 0) {
-            $($slides[i]).fadeOut();
-            $($slides[i - 1]).fadeIn();
-            i--
-        }
-    });
-
-    $nextButton.click(() => {
+    function forNext() {
         if (i < $slides.length - 1) {
             $($slides[i]).fadeOut();
             $($slides[i + 1]).fadeIn();
             i++
         }
+    }
+
+    function forPrev() {
+        if (i > 0) {
+            $($slides[i]).fadeOut();
+            $($slides[i - 1]).fadeIn();
+            i--
+        }
+    }
+
+    $prevButton.click(() => {
+        forPrev();
     });
+
+    $nextButton.click(() => {
+        forNext();
+    });
+
+    $("body").keydown(e) => {
+        if (e.keyCode == keyCodeLeft) {
+            forPrev();
+        }
+        else if (e.keyCode == keyCodeRight) {
+            forNext();
+        }
+    };
 
 });
