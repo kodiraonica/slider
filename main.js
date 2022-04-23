fcb90ab725ef7e1233412b7408db58cb277a4ac3
Valentina Otočan <valentinaotocan@gmail.com>
2022-04-23 12:18:58 +0200

    Refactoring JS

--- main.js
+++ main.js
@@ -3,21 +3,40 @@ $(document).ready(function () {
   const $prevButton = $(".slider__button--prev");
   const $nextButton = $(".slider__button--next");
 
+  const keyCodeRight = 39;
+  const keyCodeLeft = 37;
+
   let i = 0;
 
-  $prevButton.click(() => {
+  function clickPrev() {
     if (i > 0) {
       $($slides[i]).fadeOut();
       $($slides[i - 1]).fadeIn();
       i--;
     }
+  }
+
+  $prevButton.click(() => {
+    clickPrev();
   });
 
-  $nextButton.click(() => {
+  function clickNext() {
     if (i < $slides.length - 1) {
       $($slides[i]).fadeOut();
       $($slides[i + 1]).fadeIn();
       i++;
+    }
+  }
+
+  $nextButton.click(() => {
+    clickNext();
+  });
+
+  $("body").keydown(function (e) {
+    if (e.keyCode == keyCodeLeft) {
+      clickPrev();
+    } else if (e.keyCode == keyCodeRight) {
+      clickNext();
     }
   });
 });
